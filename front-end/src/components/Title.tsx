2652af410e152136ec565471242ab22493eff4c3
William Duclot <william.duclot@gmail.com>
2021-05-12 13:49:29 +0100

    Update frontend

--- front-end/src/components/Title.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-family: sans-serif;
-  font-size: 24px;
-  margin-bottom: 8px;
-`;
-
-export default Title;
--- front-end/src/store/sagas/index.ts
+++
@@ -1,5 +0,0 @@
-function* initSaga() {
-  yield [];
-}
-
-export default initSaga;
